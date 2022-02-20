bd51c3b91fb3c859b785dbc7cd0608d59d637744
Jenna Goldberg <25210118+jgoldberg1@users.noreply.github.com>
2022-02-19 19:12:05 -0800

    ui with button for carlos

--- acm_hackathon_2022/lib/main.dart
+++ acm_hackathon_2022/lib/main.dart
@@ -6,48 +6,83 @@ void main() {
   runApp(Home());
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int testRes = 0;
+  final VoidCallback onPressed = test;
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('GarlicGantt'),
-        ),
-        body: Container(
+          appBar: AppBar(
+            title: const Text('GarlicGantt'),
+          ),
+          body: Container(
+              child: Center(
             child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
-              children: const <Widget>[Text("Add a recipe")],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: <Widget>[
+                const RecipeSelector(),
+                const RecipeSelector(),
+                const RecipeSelector(),
                 FloatingActionButton(
-                  child: const Text('+'),
-                  onPressed: () {},
-                ),
+                    child: const Icon(Icons.kitchen), onPressed: () {}),
                 FloatingActionButton(
-                  child: const Text('X'),
-                  onPressed: () {},
-                )
+                    child: const Text('Carlos Button'), onPressed: onPressed),
               ],
-            )
-          ],
-        )),
-      ),
+            ),
+          ))),
     );
   }
 }
 
+class RecipeSelector extends StatefulWidget {
+  const RecipeSelector({Key? key}) : super(key: key);
 
-
-/*class _GarlicAppState extends State<GarlicApp> {
-  String msg = 'Flutter RaisedButton Example';
   @override
- 
+  State<RecipeSelector> createState() => _RecipeSelectorState();
 }
-*/
+
+class _RecipeSelectorState extends State<RecipeSelector> {
+  String dropdownValue = 'None';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownValue = newValue!;
+        });
+      },
+      items: <String>[
+        'None',
+        'Rosemary Garlic Steak',
+        'Everyday Salad',
+        'Passionfruit Mousse'
+      ].map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
+//PUT YOUR FUNCTION OVER HERE, CARLOS
+int test() {
+  print('Hello World');
+  return 1;
+}
